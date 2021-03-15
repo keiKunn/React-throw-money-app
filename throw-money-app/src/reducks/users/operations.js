5bffa3eb29f6b714b93fcfb5f4191c291eee9f06
keiKunn <k.aragn03@gmail.com>
2021-03-15 19:26:57 +0900

    レビュー反映 不要なasync記述を削除・不要なif条件削除、リファクタリング

--- throw-money-app/src/reducks/users/operations.js
+++ throw-money-app/src/reducks/users/operations.js
@@ -1,12 +1,12 @@
-import {auth,db, FirebaseTimestamp } from '../../firebase/index'
-import {push, goBack} from 'connected-react-router'
+import { auth, db, FirebaseTimestamp } from '../../firebase/index'
+import { push, goBack } from 'connected-react-router'
 import { addUserAction } from './actions';
 
 // アカウント登録ボタン押下
 export const pushRegistUser = (username, email, password, confirmPassword) => {
-  // バリデーションチェック(仮)＞firebase authを使ってユーザ作成 > firestoreにユーザ登録 > 登録後ホームへ遷移(dispatchで管理)
-  return async(dispatch) => {
-    // バリデーションチェック(仮)
+  // firestoreにユーザ登録
+  return (dispatch) => {
+    // バリデーションチェック
     if (username === "" || email === "" || password === "" || confirmPassword === "") {
       alert("必須項目が未入力です");
       return false
@@ -22,16 +22,14 @@ export const pushRegistUser = (username, email, password, confirmPassword) => {
       return false
     }
 
-    // firebase authを呼び出し、結果をreturn
-    return auth.createUserWithEmailAndPassword(email, password)
+    // firebase authを呼び出し
+    auth.createUserWithEmailAndPassword(email, password)
       .then(result => {
-      const user = result.user;
+        const user = result.user;
 
-      // ユーザが作成された場合
-      if (user) {
+        // ユーザ情報作成
         const uid = user.uid;
         const timestamp = FirebaseTimestamp.now();
-
         const userInitialData = {
           created_time: timestamp,
           email: email,
@@ -42,26 +40,22 @@ export const pushRegistUser = (username, email, password, confirmPassword) => {
         }
 
         // firestoreに登録
-        db.collection('users').doc(uid).set(userInitialData).then(()=>{
+        db.collection('users').doc(uid).set(userInitialData).then(result => {
           //登録後、state変更
           dispatch(addUserAction({
-            userName:username,
-            passWord:password
+            userName: username,
+            passWord: password
           }));
           //ホームへ遷移
           dispatch(push('/'))
         })
-
-      }
-
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(`firebase auth ユーザ登録が失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`);
-
-      return false
-    });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(`firebase auth ユーザ登録が失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`);
+        return false
+      });
   }
 
 }
