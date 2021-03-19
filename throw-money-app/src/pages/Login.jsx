a102ccc365cad87af65242fa2b969ca664176453
keiKunn <k.aragn03@gmail.com>
2021-03-19 20:28:38 +0900

    Prettier導入 Login.jsx と operation.jsxにフォーマット適用

--- throw-money-app/src/pages/Login.jsx
+++ throw-money-app/src/pages/Login.jsx
@@ -1,38 +1,57 @@
 import React, { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { push } from 'connected-react-router'
-import { login } from '../reducks/users/operations'
+import { push } from 'connected-react-router';
+import { login } from '../reducks/users/operations';
 import { InputTextForm, Button } from '../component/';
 
 const Login = () => {
   const dispatch = useDispatch();
   // ログイン画面のstateを定義
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const inputEmail = useCallback((event) => {
-    setEmail(event.target.value)
-  }, [setEmail]);
+  const inputEmail = useCallback(
+    (event) => {
+      setEmail(event.target.value);
+    },
+    [setEmail]
+  );
 
-  const inputPassword = useCallback((event) => {
-    setPassword(event.target.value)
-  }, [setPassword]);
-  
+  const inputPassword = useCallback(
+    (event) => {
+      setPassword(event.target.value);
+    },
+    [setPassword]
+  );
+
   return (
     <div>
       <h2>ログイン</h2>
-      <InputTextForm name="Email" labelName="メールアドレス" type="email"
-        value={email} onChange={inputEmail} />
+      <InputTextForm
+        name='Email'
+        labelName='メールアドレス'
+        type='email'
+        value={email}
+        onChange={inputEmail}
+      />
 
-      <InputTextForm name="Password" labelName="パスワード" type="password"
-        value={password} onChange={inputPassword} />
+      <InputTextForm
+        name='Password'
+        labelName='パスワード'
+        type='password'
+        value={password}
+        onChange={inputPassword}
+      />
 
-      <Button value="ログイン" onClick={() => dispatch(login(email, password))} />
+      <Button
+        value='ログイン'
+        onClick={() => dispatch(login(email, password))}
+      />
 
       <button onClick={() => dispatch(push('/Regist'))}>
         新規登録はこちら
       </button>
     </div>
-  )
-}
+  );
+};
 export default Login;
--- throw-money-app/src/reducks/users/operations.js
+++ throw-money-app/src/reducks/users/operations.js
@@ -1,5 +1,5 @@
-import { auth, db, FirebaseTimestamp } from '../../firebase/index'
-import { push, goBack } from 'connected-react-router'
+import { auth, db, FirebaseTimestamp } from '../../firebase/index';
+import { push, goBack } from 'connected-react-router';
 import { addUserAction, loginAction } from './actions';
 
 // アカウント登録ボタン押下
@@ -7,24 +7,30 @@ export const pushRegistUser = (username, email, password, confirmPassword) => {
   // firestoreにユーザ登録
   return (dispatch) => {
     // バリデーションチェック
-    if (username === "" || email === "" || password === "" || confirmPassword === "") {
-      alert("必須項目が未入力です");
-      return false
+    if (
+      username === '' ||
+      email === '' ||
+      password === '' ||
+      confirmPassword === ''
+    ) {
+      alert('必須項目が未入力です');
+      return false;
     }
 
     if (password !== confirmPassword) {
-      alert("パスワードが一致していません");
-      return false
+      alert('パスワードが一致していません');
+      return false;
     }
 
     if (password.length < 6) {
-      alert('パスワードは6文字以上で入力してください。')
-      return false
+      alert('パスワードは6文字以上で入力してください。');
+      return false;
     }
 
     // firebase authを呼び出し
-    auth.createUserWithEmailAndPassword(email, password)
-      .then(result => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
         const user = result.user;
 
         // ユーザ情報作成
@@ -33,65 +39,81 @@ export const pushRegistUser = (username, email, password, confirmPassword) => {
         const userInitialData = {
           created_time: timestamp,
           email: email,
-          role: "user",
+          role: 'user',
           uid: uid,
           updated_time: timestamp,
-          username: username
-        }
+          username: username,
+        };
 
         // firestoreに登録
-        db.collection('users').doc(uid).set(userInitialData).then(result => {
-          //登録後、state変更
-          dispatch(addUserAction({
-            userName: username,
-            passWord: password
-          }));
-          //ホームへ遷移
-          dispatch(push('/'))
-        })
+        db.collection('users')
+          .doc(uid)
+          .set(userInitialData)
+          .then((result) => {
+            //登録後、state変更
+            dispatch(
+              addUserAction({
+                userName: username,
+                passWord: password,
+              })
+            );
+            //ホームへ遷移
+            dispatch(push('/'));
+          });
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert(`ユーザ登録が失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`);
-        return false
+        alert(
+          `ユーザ登録が失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`
+        );
+        return false;
       });
-  }
-}
+  };
+};
 
 // ログイン
 export const login = (email, password) => {
   return (dispatch) => {
     // バリデーションチェック
-    if (email === "" || password === "") {
-      alert("必須項目が未入力です");
-      return false
+    if (email === '' || password === '') {
+      alert('必須項目が未入力です');
+      return false;
     }
 
-    auth.signInWithEmailAndPassword(email, password).then(result => {
-      const user = result.user;
-      const uid = user.uid;
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+        const uid = user.uid;
 
-      // ユーザ情報取得
-      db.collection('users').doc(uid).get().then(snapshot => {
-        const data = snapshot.data();
-        // データ取得後、state変更
-        // (action引数のstateのキーは、ユーザ新規登録処理のuserInitialDataのキーを使用すること)
-        dispatch(loginAction({
-          role: data.role,
-          uid: uid,
-          userName: data.username
-        }))
-        //ダッシュボードへ遷移
-        dispatch(push('/Dashboard'))
+        // ユーザ情報取得
+        return db
+          .collection('users')
+          .doc(uid)
+          .get()
+          .then((snapshot) => {
+            const data = snapshot.data();
+            // データ取得後、state変更
+            // (action引数のstateのキーは、ユーザ新規登録処理のuserInitialDataのキーを使用すること)
+            dispatch(
+              loginAction({
+                role: data.role,
+                uid: uid,
+                userName: data.username,
+              })
+            );
+            //ダッシュボードへ遷移
+            dispatch(push('/Dashboard'));
+          });
       })
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(`ログインが失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`);
-      return false
-    });
-  }
-
-}
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(
+          `ログインが失敗しました。| errorCode:${errorCode}, errorMessage:${errorMessage}`
+        );
+        return false;
+      });
+  };
+};
